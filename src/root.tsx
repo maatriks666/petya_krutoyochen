3b1fd4fe73b7b1125744e8484fff553972910c03
maatriks666 <130364673+maatriks666@users.noreply.github.com>
2023-04-11 00:28:44 +1100

    <A href="/">попа</A>

--- src/root.tsx
+++ src/root.tsx
@@ -25,7 +25,7 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
+            <A href="/">попа</A>
             <A href="/about">About</A>
             <Routes>
               <FileRoutes />
